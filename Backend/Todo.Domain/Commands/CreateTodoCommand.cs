6eec94773ef2d047360f07fe0a92acc5e89a58d7
FelipeAlves99 <falves99.dev@gmail.com>
2021-05-12 19:33:25 -0300

    Command to create a todo item added

---
+++ Backend/Todo.Domain/Commands/CreateTodoCommand.cs
@@ -0,0 +1,27 @@
+using System;
+using Flunt.Notifications;
+using Todo.Domain.Commands.Contracts;
+
+namespace Todo.Domain.Commands
+{
+    public class CreateTodoCommand : Notifiable, ICommand
+    {
+        public CreateTodoCommand() { }
+
+        public CreateTodoCommand(string title, DateTime date, string user)
+        {
+            Title = title;
+            Date = date;
+            User = user;
+        }
+
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string User { get; set; }
+
+        public void Validate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
