de841b2bc8476196fb2abe44aac377b4302e52f0
FelipeAlves99 <falves99.dev@gmail.com>
2021-05-13 20:10:53 -0300

    Added get by id query

--- Backend/Todo.Domain/Queries/TodoQueries.cs
+++ Backend/Todo.Domain/Queries/TodoQueries.cs
@@ -28,6 +28,13 @@ namespace Todo.Domain.Queries
                 x.Done == done &&
                 x.Date.Date == date.Date;
         }
+
+        public static Expression<Func<TodoItem, bool>> GetById(Guid id, string user)
+        {
+            return x => 
+                x.Id == id &&
+                x.User == user;
+        }
     }
 
 }
